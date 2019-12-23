d5dc2012ee2a7697830d0d7ead4a73410700776f
ren.meta@gmail.com <ren.meta@gmail.com>
2019-12-22 22:50:31 -0500

    fixed deployment on development, author in models was not in quotes

--- server/models/post.js
+++ server/models/post.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
 
     Post.belongsTo(models.User, {
       foreignKey: 'userId',
-      as: author,
+      as: 'author',
       onDelete: 'CASCADE'
     })
   };
